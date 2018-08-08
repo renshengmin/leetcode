80a14ecef93687a5140b92eeff24789cdce3ffa6
mistifler <mistifler@126.com>
2018-08-08 13:29:27 +0800

    199 AC

---
+++ src/199 Binary Tree Right Side View/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (null == root) {
+            return ret;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int curLevel = q.size();
+            TreeNode curNode;
+            for(int i =0; i<curLevel; ++i) {
+                curNode = q.poll();
+                if(curNode.left != null) {
+                    q.offer(curNode.left);
+                }
+                if(curNode.right != null) {
+                    q.offer(curNode.right);
+                }
+            }
+            ret.add(curNode.val);
+        }
+        return ret;
+    }
+}
