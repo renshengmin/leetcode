b1ba2caab14c08d86a98b4a6b89a314c8f353140
mistifler <mistifler@126.com>
2018-08-09 13:44:53 +0800

    AC 328 Odd Even Linked List

---
+++ src/328 Odd Even Linked List/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (null == head || null == head.next) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode evenHead = head.next;
+
+		//遍历
+        ListNode oddPtr = head;
+        ListNode evenPtr = head.next;
+        while (evenPtr != null && evenPtr.next != null) {
+        	oddPtr.next = evenPtr.next;
+        	evenPtr.next = evenPtr.next.next;
+        	oddPtr = oddPtr.next;
+        	evenPtr = evenPtr.next;
+        }
+
+        oddPtr.next = evenHead;
+
+        return dummy.next;
+    }
+}
