0ee218c2d317a4be449ab5b39a5c7ad60547fd88
mistifler <mistifler@126.com>
2018-08-13 13:30:08 +0800

    AC 817 Linked List Components

--- src/817 Linked List Components/Solution.java
+++ src/817 Linked List Components/Solution.java
@@ -8,6 +8,23 @@
  */
 class Solution {
     public int numComponents(ListNode head, int[] G) {
-        
+        Set<Integer> set = new HashSet<>();
+        for (int i : G) {
+            set.add(i);
+        }
+
+        int ret = 0;
+        ListNode p = head;
+        while (p != null) {
+            if (set.contain(p.val)) {
+                while (p != null && set.contain(p.val)) {
+                    p = p.next;
+                }
+                ret++;
+            }
+            p = p.next;
+        }
+
+        return ret;
     }
 }
