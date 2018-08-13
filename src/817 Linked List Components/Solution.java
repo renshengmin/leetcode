a60ec2a8953548e356612564291e776028421afb
mistifler <mistifler@126.com>
2018-08-13 13:34:26 +0800

    Update Solution.java

--- src/817 Linked List Components/Solution.java
+++ src/817 Linked List Components/Solution.java
@@ -7,7 +7,32 @@
  * }
  */
 class Solution {
+    
+    //根据题目限制可以直接开一个数组作为HashSet，运行表现更好一点
     public int numComponents(ListNode head, int[] G) {
+        boolean[] set = new boolean[10000];
+        for(Integer i : G) {
+            set[i] =  true;
+        }
+
+        int ret = 0;
+        ListNode p = head;
+        while (p != null ) {
+            if (set[p.val]) {
+                while (p.next!=null && set[p.val]) {
+                    p = p.next;
+                }
+                ret++;
+            }
+            p = p.next;
+        }
+
+        return ret;
+    }
+    
+    
+    //其他参考：使用HaseSet
+    public int numComponents2(ListNode head, int[] G) {
         Set<Integer> set = new HashSet<>();
         for (int i : G) {
             set.add(i);
