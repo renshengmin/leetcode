99939d1df02f7b6ae9008c9033894df8944fc39c
ICTMi <mistifler@126.com>
2018-08-14 13:12:24 +0800

    AC 94 recursion solution

--- src/94 Binary Tree Inorder Traversal/Solution.java
+++ src/94 Binary Tree Inorder Traversal/Solution.java
@@ -7,8 +7,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+//递归的解法
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        
+        List<Integer> ret = new ArrayList<>();
+        traversalHelper(root, ret);
+        return ret;   
+    }
+
+    public void traversalHelper(TreeNode root, List<Integer> ret) {
+        if(root == null) {
+            return;
+        }
+        traversalHelper(root.left, ret);
+        ret.add(root.val);
+        traversalHelper(root.right, ret);
     }
 }
