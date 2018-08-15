c0f242b1d5502c041e640a3a7bb81e4a4215d718
ICTMi <mistifler@126.com>
2018-08-15 13:09:02 +0800

    AC 144 recursion solution

--- src/144 Binary Tree Preorder Traversal/Solution.java
+++ src/144 Binary Tree Preorder Traversal/Solution.java
@@ -9,6 +9,18 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        
+        List<Integer> ret = new ArrayList<>();
+        traversalHelper(root, ret);
+        return ret;
+    }
+
+    public void traversalHelper(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+
+        ret.add(root.val);
+        traversalHelper(root.left, ret);
+        traversalHelper(root.right, ret);
     }
 }
