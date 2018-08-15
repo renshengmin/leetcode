828fc584dca502d1c4b1f78e94f5b753b98645e3
ICTMi <mistifler@126.com>
2018-08-15 13:45:12 +0800

    AC 145 recursion solution

--- src/145 Binary Tree Postorder Traversal/Solution.java
+++ src/145 Binary Tree Postorder Traversal/Solution.java
@@ -9,6 +9,18 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        
+        List<Integer> ret = new ArrayList<>();
+        traversalHelper(root, ret);
+        return ret;
+    }
+
+    public void traversalHelper(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+
+        traversalHelper(root.left, ret);
+        traversalHelper(root.right, ret);
+        ret.add(root.val);
     }
 }
