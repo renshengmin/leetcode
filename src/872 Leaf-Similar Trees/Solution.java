f35986015f1bea09057646218c3a0780190704f4
ICTMi <mistifler@126.com>
2018-08-16 13:26:43 +0800

    AC 872. Leaf-Similar Trees

--- src/872 Leaf-Similar Trees/Solution.java
+++ src/872 Leaf-Similar Trees/Solution.java
@@ -9,6 +9,23 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        
+        List<Integer> ret1 = new ArrayList<>();
+        List<Integer> ret2 = new ArrayList<>();
+        preOrderTraverse(root1, ret1);
+        preOrderTraverse(root2, ret2);
+
+        return ret1.equals(ret2);
+    }
+
+    public void preOrderTraverse(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            ret.add(root.val);
+        }
+        preOrderTraverse(root.left, ret);
+        preOrderTraverse(root.right, ret);
     }
 }
