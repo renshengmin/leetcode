26b3fd71003f8c172e4c7364c066c2a00f9eed91
ICTMi <mistifler@126.com>
2018-08-21 13:42:43 +0800

    AC 701 recursive

---
+++ src/701 Insert into a Binary Search Tree/Solution2.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (null == root) {
+            return new TreeNode(val);
+        }
+        if(root.val == val) {
+            return root;
+        }
+        if(root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
