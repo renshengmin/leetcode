89c95597cb2c04a0439277fcd1416405da6e9ff1
ICTMi <mistifler@126.com>
2018-08-21 13:26:07 +0800

    AC 701

--- src/701 Insert into a Binary Search Tree/Solution.java
+++ src/701 Insert into a Binary Search Tree/Solution.java
@@ -9,6 +9,33 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        TreeNode cur = root;
+        TreeNode node = new TreeNode(val);
+        while (true) {
+            if (cur.val == val) {
+                return root;
+            }
+
+            if (cur.val < val) {
+                if (cur.right == null) {
+                    cur.right = node;
+                    break;
+                } else {
+                    cur = cur.right;
+                }
+            } else {
+                if (cur.left == null) {
+                    cur.left = node;
+                    break;
+                } else {
+                    cur = cur.left;
+                }
+            }
+        }
+        return root;
     }
 }
