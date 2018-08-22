6d57ae82d60395d89a6b868f4e7e31f44c5b450f
ICTMi <mistifler@126.com>
2018-08-22 13:35:11 +0800

    AC 814

--- src/814 Binary Tree Pruning/Solution.java
+++ src/814 Binary Tree Pruning/Solution.java
@@ -9,6 +9,16 @@
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        
+        if (null == root) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (null == root.left && null == root.right && root.val == 0) {
+            return null;
+        }
+
+        return root;
     }
 }
