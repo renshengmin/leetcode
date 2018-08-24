cf6bb9878a4ce15969cc46362ef6ae760ff9fe57
ICTMi <mistifler@126.com>
2018-08-24 22:43:43 +0800

    AC 687

--- src/687 Longest Univalue Path/Solution.java
+++ src/687 Longest Univalue Path/Solution.java
@@ -8,7 +8,31 @@
  * }
  */
 class Solution {
+    int ret;
     public int longestUnivaluePath(TreeNode root) {
+        ret = 0;
+        postOrder(root);
+        return ret;
+    }
+    
+    public int postOrder(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftDepth = postOrder(root.left);
+        int rightDepth = postOrder(root.right);
+        
+        int lhs = 0;
+        int rhs = 0;
+        if (root.left != null && root.val == root.left.val) {
+            lhs = leftDepth + 1;
+        }
+        if (root.right != null && root.val == root.right.val) {
+            rhs = rightDepth + 1;
+        }
+        ret = Math.max(ret, lhs + rhs);
+        return Math.max(lhs, rhs);
         
     }
 }
