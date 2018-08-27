b4247cb3a0e82577391c479f5be243d822280f4a
mistifler <mistifler@126.com>
2018-08-27 23:39:43 +0800

    AC 257

--- src/257 Binary Tree Paths/Solution.java
+++ src/257 Binary Tree Paths/Solution.java
@@ -9,8 +9,27 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        
+        List<String> ret = new ArrayList<>();
+        String path = "";
+        dfs(ret, path, root);
+        return path;
     }
-    
-    
+
+    public void dfs(List<String> ret, String path, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            path += node.val + "";
+            ret.add(path);
+        }
+
+        if (node.left != null) {
+            dfs(ret, path + node.val + "->", node.left);
+        }
+        if (node.right != null) {
+            dfs(ret, path + node.val + "->", node.right);
+        }
+    }
+
 }
