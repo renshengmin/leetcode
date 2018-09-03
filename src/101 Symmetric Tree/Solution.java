a2d80c780afd84682e10fe456de49e7d6de2dadc
ICTMi <mistifler@126.com>
2018-09-03 23:26:20 +0800

    AC 101

--- src/101 Symmetric Tree/Solution.java
+++ src/101 Symmetric Tree/Solution.java
@@ -9,6 +9,22 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
         
+        return symmetricHelper(root.left, root.right);
+    }
+    
+    public boolean symmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        
+        if (left == null || right == null) {
+            return false;
+        }
+        
+        return left.val == right.val && symmetricHelper(left.right, right.left) && symmetricHelper(left.left, right.right);
     }
 }
