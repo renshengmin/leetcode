7c168d6af83f3205fa33a9590f657c2b6c6a1ee4
ICTMi <mistifler@126.com>
2018-09-05 13:11:31 +0800

    AC 671

--- src/671 Second Minimum Node In a Binary Tree/Solution.java
+++ src/671 Second Minimum Node In a Binary Tree/Solution.java
@@ -8,7 +8,23 @@
  * }
  */
 class Solution {
+    int minVal;
+    long ret = Long.MAX_VALUE;
+    
     public int findSecondMinimumValue(TreeNode root) {
-        
+        minVal = root.val;
+        dfs(root);
+        return ret < Long.MAX_VALUE ? (int)ret : -1;
+    }
+    
+    public void dfs(TreeNode root) {
+        if (root != null) {
+            if (minVal < root.val && root.val < ret) {
+                ret = root.val;
+            } else if (minVal == root.val) {
+                dfs(root.left);
+                dfs(root.right);
+            }
+        }
     }
 }
