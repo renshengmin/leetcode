dfae3409d5eb327c1c199f3bfd0c3edc787869bb
mistifler <mistifler@126.com>
2018-09-06 22:46:43 +0800

    AC 662

--- src/662 Maximum Width of Binary Tree/Solution.java
+++ src/662 Maximum Width of Binary Tree/Solution.java
@@ -8,7 +8,31 @@
  * }
  */
 class Solution {
+    int ret;
+    Map<Integer, Integer> depthLeft;
+    
     public int widthOfBinaryTree(TreeNode root) {
-        
+        if (null == root) {
+            return 0;
+        }
+        ret = 1;
+        depthLeft = new HashMap<>();
+        dfs(root, 1, 1);
+        return ret;
+    }
+
+    public void dfs(TreeNode node, int depth, int idx) {
+        if (null == node) {
+            return;
+        }
+
+        if (!depthLeft.containsKey(depth)) {
+            depthLeft.put(depth, idx);
+        } else {
+            ret = Math.max(ret, idx - depthLeft.get(depth) + 1);
+        }
+        dfs(node.left, depth + 1, 2 * idx);
+        dfs(node.right, depth + 1, 2 * idx + 1);
+
     }
 }
