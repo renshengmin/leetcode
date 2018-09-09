223804b742ce1568e78c74ea77a9ad0d289f3c85
ICTMi <mistifler@126.com>
2018-09-09 22:20:42 +0800

    AC 103

--- src/103 Binary Tree Zigzag Level Order Traversal/Solution.java
+++ src/103 Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -9,6 +9,34 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        
+        List<List<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean reverseFlag = false;
+        if (root == null) {
+            return ret;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> levelRet = new ArrayList<>();
+            for (int i = 0; i < levelNum; ++i) {
+                TreeNode curNode = queue.poll();
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+
+                if(reverseFlag) {
+                    levelRet.add(0, curNode.val);
+                }else{
+                    levelRet.add(curNode.val);
+                }
+            }
+            ret.add(levelRet);
+            reverseFlag = !reverseFlag;
+        }
+        return ret;
     }
 }
