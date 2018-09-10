5940b1981dfa46fd820ab74a1a72b57e6c35800d
ICTMi <mistifler@126.com>
2018-09-10 22:55:12 +0800

    AC 107

--- src/107 Binary Tree Level Order Traversal II/Solution.java
+++ src/107 Binary Tree Level Order Traversal II/Solution.java
@@ -9,6 +9,27 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
+        List<List<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) {
+            return ret;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> levelRet = new ArrayList<>();
+            for(int i=0; i<levelNum; ++i) {
+                TreeNode curNode = queue.poll();
+                if(curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if(curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+                levelRet.add(curNode.val);
+            }
+            ret.add(0, levelRet);
+        }
+        return ret;
     }
 }
