a107ef1bfeebcedf57a9351fdf93f1c02fea60c9
ICTMi <mistifler@126.com>
2018-09-12 00:34:41 +0800

    AC 105

--- src/105 Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
+++ src/105 Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
@@ -9,6 +9,24 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+        return buildTreeHelper(preorder, 0, inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode buildTreeHelper(int[] preorder, int curRootIdx, int[] inorder, int inStart, int inEnd) {
+        if (curRootIdx > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }   
+
+        TreeNode root = new TreeNode(preorder[curRootIdx]);
+        int index = 0;
+        for (int i=inStart; i<=inEnd; i++) {
+            if (inorder[i] == preorder[curRootIdx]) {
+                index = i;
+                break;
+            }
+        }
+        root.left = buildTreeHelper(preorder, curRootIdx + 1, inorder, inStart, index - 1);
+        root.right = buildTreeHelper(preorder, curRootIdx + index - inStart + 1, inorder, index + 1, inEnd);
+        return root;
     }
 }
