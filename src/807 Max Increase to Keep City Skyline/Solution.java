b56a0a50f7bee7c50ad203d3d3fa135181ac7ec5
ICTMi <mistifler@126.com>
2018-09-28 23:10:53 +0800

    AC

--- src/807 Max Increase to Keep City Skyline/Solution.java
+++ src/807 Max Increase to Keep City Skyline/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        
+        if (grid == null) {
+            return 0;
+        }
+        int ret = 0;
+        final int m = grid.length;
+        final int n = grid[0].length;
+        int[] colMax = new int[n];
+        int[] rowMax = new int[m];
+        for(int i=0; i<m; i++) {
+            int curRowMax = grid[i][0];
+            for(int j=0; j<n; j++) {
+                curRowMax = Math.max(curRowMax, grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+            rowMax[i] = curRowMax;
+        }
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+            	int line = Math.min(rowMax[i], colMax[j]);
+                ret += line > grid[i][j] ? line - grid[i][j] : 0;
+            }
+        }
+        return ret;
     }
 }
